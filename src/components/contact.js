02fc1b8ac84763e2753abef5a923960a1a1d585f
jz <macgyver@users.noreply.github.com>
2016-10-11 19:06:41 -0700

    updated hours

--- src/components/contact.js
+++ src/components/contact.js
@@ -19,8 +19,9 @@ export default function Contact(props) {
 				rel="noopener"
 			>map</a>
 			<div className="hours">
-				<time itemProp="openingHours" dateTime="Fri 16:00-23:00">Fri 4-11</time>
-				<time itemProp="openingHours" dateTime="Sat 12:00-23:00">Sat 12-11</time>
+				<time itemProp="openingHours" dateTime="Thu 16:00-22:00">Thu 4-10</time>
+				<time itemProp="openingHours" dateTime="Fri 16:00-24:00">Fri 4-12</time>
+				<time itemProp="openingHours" dateTime="Sat 13:00-24:00">Sat 1-12</time>
 				<time itemProp="openingHours" dateTime="Sun 12:00-21:00">Sun 12-9</time>
 			</div>
 			<a itemProp="telephone" href="tel:+14155902250">(415) 590-2550</a>
